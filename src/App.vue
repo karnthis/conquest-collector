865af490cb6017167527f6c11015ebaa195eebd8
Erin RIvas <erin@atomicbear.com>
2021-01-17 09:17:32 -0600

    Initial form build complete, ready to wire up

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,163 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-row class="mt-10">
+      <v-col cols="12" md="4"></v-col>
+      <v-col cols="12" md="4">
+        <v-card>
+          <v-card-title>
+            <h1>Game Submission Form</h1>
+          </v-card-title>
+          <v-card-text>
+
+            <v-row><h2>Player Names</h2></v-row>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="p1.name"
+                  :rules="isRequired"
+                  label="Player One"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="p2.name"
+                  :rules="isRequired"
+                  label="Player Two"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row><h2>Win / Loss / Draw / Yield</h2></v-row>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="p1.wldy"
+                  :rules="isRequired"
+                  label="Player One"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="p2.wldy"
+                  :rules="isRequired"
+                  label="Player Two"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col></v-col>
+            </v-row>
+
+            <v-row><h2>Points Played / Lost</h2></v-row>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="game.points"
+                  :rules="isRequired"
+                  label="Starting Points"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="p1.lost"
+                  label="P1 Points Lost"
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="p2.lost"
+                  label="P2 Points Lost"
+                  outlined
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row><h2>Victory Points</h2></v-row>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="p1.vp"
+                  :rules="isRequired"
+                  label="Player One"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="p2.vp"
+                  :rules="isRequired"
+                  label="Player Two"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="game.vp"
+                  :rules="isRequired"
+                  label="Required"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+            </v-row>
+
+            <v-row><h2>Rounds</h2></v-row>
+            <v-row>
+              <v-col>
+                <v-text-field
+                  v-model="game.end"
+                  :rules="isRequired"
+                  label="Round Finished"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field
+                  v-model="game.max"
+                  :rules="isRequired"
+                  label="Round Limit"
+                  required
+                  outlined
+                ></v-text-field>
+              </v-col>
+              <v-col></v-col>
+            </v-row>
+
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="4"></v-col>
+    </v-row>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import SingleFormField from '@/components/SingleFormField'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {},
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+  data: () => ({
+    p1: {},
+    p2: {},
+    game: {},
+    isRequired: [
+      v => !!v || 'Field is Required'
+    ]
+  })
 }
-</style>
+</script>
